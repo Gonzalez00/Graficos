36880c693146190f229ded7e7b45dc7f56f43fbb
Gonzalez00 <gonzalez.leiva00@gmail.com>
2024-10-31 09:09:15 -0600

    Graficos1

---
+++ Firebase/FirebaseConfig.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDLjpYfNtQPDzAmlAWmFXIilqS7GGRzvlk",
+    authDomain: "startcap-ef1bf.firebaseapp.com",
+    projectId: "startcap-ef1bf",
+    storageBucket: "startcap-ef1bf.appspot.com",
+    messagingSenderId: "279595376753",
+    appId: "1:279595376753:web:19a9fbadd0fea95f9abbd0",
+    measurementId: "G-GNGDKNF149"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+  
+// Inicializa Firestore
+const db = getFirestore(app);
+  
+  export default db;
+
+
+
